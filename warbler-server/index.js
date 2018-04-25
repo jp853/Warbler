b8d3c458b3659ed5d35efcd8b3c9babe50e8b7d2
jp853 <jpingatore@gmail.com>
2018-04-25 09:33:30 -0600

    index.js boiler plate

---
+++ warbler-server/index.js
@@ -0,0 +1,23 @@
+/*jshint esversion: 6 */
+
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const PORT = 8081;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+// all routes here - later
+
+app.use(function(req, res, next) {
+  let err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+app.listen(PORT, function(){
+  console.log('Server is starting on port ${PORT}');
+});
