1014357f2dc6cc2dc1f17524bb2705700217ab26
jp853 <jpingatore@gmail.com>
2018-04-25 10:10:12 -0600

    PASSWORD IS NOW HASHED

--- warbler-server/models/user.js
+++ warbler-server/models/user.js
@@ -26,6 +26,31 @@ const userSchema = new mongoose.Schema({
   }
 });
 
+// Hash the password
+userSchema.pre("save", async function(next) {
+  try {
+    if(!this.isModified("password")) {
+      return next();
+    }
+    let hashedPassword = await bcrypt.hash(this.password, 10);
+    this.password = hashedPassword;
+    return next();
+  } catch(err) {
+    return next(err);
+  }
+});
+
+
+// Check to see user has put in correct password
+userSchema.method.comparePassword = async function(candidatePassword, next) {
+  try {
+    let isMatch = await bcrypt.compare(candidatePassword, this.password);
+    return isMatch;
+  } catch (err) {
+    return next(err);
+  }
+};
+
 const User = mongoose.model("User", userSchema);
 
 module.exports = User;
