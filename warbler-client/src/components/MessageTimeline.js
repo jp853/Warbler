e7b916ea9339e05a41d0b25ddbb3d455fd3db426
jp853 <jpingatore@gmail.com>
2018-04-26 14:11:23 -0600

    MessageTimeline render

---
+++ warbler-client/src/components/MessageTimeline.js
@@ -0,0 +1,18 @@
+/*jshint esversion: 6 */
+import React from "react";
+import MessageList from "../containers/MessageList";
+import UserAside from "./UserAside";
+
+const MessageTimeline = props => {
+  return (
+    <div className="row">
+      <UserAside
+        profileImageUrl={props.profileImageUrl}
+        username={props.username}
+      />
+      <MessageList />
+    </div>
+  );
+};
+
+export default MessageTimeline;
