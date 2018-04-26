72b0d79ae3538da625b6506a2b9a594a103610f2
jp853 <jpingatore@gmail.com>
2018-04-26 14:14:27 -0600

    Container/App.js import and component render

--- warbler-client/src/containers/App.js
+++ warbler-client/src/containers/App.js
@@ -1,21 +1,34 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+/*jshint esversion: 6 */
+import React, { Component } from "react";
+import { Provider } from "react-redux";
+import { configureStore } from "../store";
+import { BrowserRouter as Router } from "react-router-dom";
+import Navbar from "./Navbar";
+import Main from "./Main";
+import { setAuthorizationToken, setCurrentUser } from "../store/actions/auth";
+import jwtDecode from "jwt-decode";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+const store = configureStore();
+
+if (localStorage.jwtToken) {
+  setAuthorizationToken(localStorage.jwtToken);
+  // prevent someone from manually tampering with the key of jwtToken in localStorage
+  try {
+    store.dispatch(setCurrentUser(jwtDecode(localStorage.jwtToken)));
+  } catch (e) {
+    store.dispatch(setCurrentUser({}));
   }
 }
+
+const App = () => (
+  <Provider store={store}>
+    <Router>
+      <div className="onboarding">
+        <Navbar />
+        <Main />
+      </div>
+    </Router>
+  </Provider>
+);
 
 export default App;
